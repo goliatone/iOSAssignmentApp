436bc0107748220c71525bc290312f6d2284efa2
goliatone <burgosemi@gmail.com>
2017-12-20 00:58:42 +0100

    Add server

---
+++ server/routes/api.js
@@ -0,0 +1,62 @@
+"use strict";
+const router = require("express").Router();
+const quotes = require('seinfeld-quotes');
+const Persistence = require("../lib/persistence");
+
+module.exports.init = function(app, config) {
+    ////////////////////////////////////////////////
+    // User management endpoints
+    ////////////////////////////////////////////////
+
+    router.get("/user/:id", (req, res, next) => {
+        const id = req.params.id;
+
+        Persistence.findUserById(id)
+            .then(user => {
+                res.send({
+                    success: true,
+                    values: user
+                });
+            })
+            .catch(next);
+    });
+
+    router.put("/user/:id", (req, res, next) => {
+        const id = req.params.id;
+        const token = req.query.access_token;
+
+        Persistence.findUserByToken(token)
+            .then((user = {}) => {
+                if (user.id !== id){
+                    return next(new app.AppError("Unauthorized", 401));
+                }
+
+                Persistence.updateUser(id, req.body)
+                    .then(user => {
+                        res.send({
+                            success: true,
+                            values: user
+                        });
+                    })
+                    .catch(next);
+            })
+            .catch(next);
+    });
+
+    router.get("/user/:id/om", (req, res, next) => {
+        const id = req.params.id;
+        const token = req.query.access_token;
+
+        Persistence.findUserByToken(token)
+            .then((user = {}) => {
+                if (user.id !== id){
+                    return next(new app.AppError("Unauthorized", 401));
+                }
+
+                res.send(quotes());
+            })
+            .catch(next);
+    });
+
+    app.use("/api", router);
+};
---
+++ server/routes/invite.js
@@ -0,0 +1,61 @@
+'use strict';
+const router = require('express').Router();
+const fs = require('fs');
+const marked = require('marked');
+const filepath = '../README.md';
+const useragent = require('useragent');
+
+const Persistence = require('../lib/persistence');
+
+module.exports.init = function (app) {
+
+    app.get('/', function (req, res) {
+        fs.readFile(filepath, 'utf8', function (err, data) {
+            if (err) {
+                app.logger.error(err);
+            }
+            const content = marked(data.toString());
+            res.render('index', { content });
+        });
+
+    });
+    /**
+     * Generate invite code for a given 
+     * user.
+     */
+    router.get('/email/:user', (req, res, next) => {
+        const id = req.params.user;
+        const debug = req.query.debug;
+        console.log('debug', debug)
+        const ua = useragent.is(req.headers['user-agent']);
+
+        Persistence.generateInviteCodeForUser(id).then((user) => {
+            let iosLink = false;
+
+            if (debug || ua['mobile_safari']) {
+                iosLink = 'myom://signup?token=' + user.inviteCode;
+            } 
+
+            res.render('invite', {
+                user,
+                iosLink
+            });
+            
+        }).catch(next);
+    });
+
+    router.post('/ios/:invite', (req, res, next) => {
+        const invite = req.params.invite;
+        Persistence.findUserByInviteCode(invite).then((user) => {
+            delete user.inviteCode;
+            user.registered = true;
+
+            res.send({
+                success: true,
+                values: user
+            });
+        }).catch(next);
+    });
+
+    app.use('/invite', router);
+};
